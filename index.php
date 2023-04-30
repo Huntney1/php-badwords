ba8f797a28f507e223fc8d9cd272be1f875a337e
Huntney1 <devilwhiteone@hotmail.it>
2023-04-30 12:58:39 +0200

    fix input name & surname

--- index.php
+++ index.php
@@ -31,54 +31,57 @@ error_reporting(E_ALL);
 <body style="background-color: #5800FF;">
     <div class="container-xl">
         <div class="row d-flex justify-content-center" id="margin-row">
-            <div class="col-10 d-flex justify-content-center align-items-center bg-black text-info" id="container-box"> <!-- top-50 start-50 translate-middle -->
+            <div class="col-10 py-5 d-flex justify-content-center align-items-center bg-black text-info" id="container-box"> <!-- top-50 start-50 translate-middle -->
                 <!-- in 'action' ci va solo il 'path' (nome) del fail che voglio raggiungere  -->
                 <!-- method [GET] 'espone i dati nell'URL' -->
                 <form method="POST" action="./processing.php">
 
-                    <div class="col">
+                    <div class="continer-lg">
+
                         <!-- input con anagrafica (Nome & Cognome)-->
-                        <div class="anagrafe">
+                        <div class="anagrafe row">
 
                             <div class="anagInput col-12 d-flex justify-content-around text-center align-middle mt-3">
 
                                 <div class="mb-3">
 
-                                    <label for="name" class="form-label"> Nome </label>
-                                    <input class="form-control anagInpN rounded-pill border border-3 border-info" type="text" id="nome" name="nome" aria-describedby="name" placeholder="Nome">
+                                    <div class="col-12 d-flex justify-content-between gap-4">
 
+                                        <div>
+                                            <label for="name" class="form-label"> Nome </label>
+                                            <input class="form-control rounded-pill border border-3 border-info" type="text" id="nome" name="nome" placeholder="Nome">
+                                        </div>
 
+                                        <div>
+                                            <label for="name" class="form-label"> Cognome </label>
+                                            <input class="form-control rounded-pill border border-3 border-info" type="text" id="cognome" name="cognome" placeholder="Cognome">
+                                        </div>
+                                        
+                                    </div>
                                 </div>
-                                <div class="mb-3"></div>
-
-
-
-                                <input class="anagInpC" type="text" id="cognome" name="cognome" placeholder="Cognome">
-                                <label for="cognome"> Cognome </label>
                             </div>
                         </div>
+
+                        <br>
+
+                        <div class="censured">
+                            <!-- parola da censurare censurata -->
+                            <input type="text" id="censored_word" name="censored_word" placeholder="Inserisci Censura">
+                            <label for="censored_word"> Parola da censurare </label>
+                        </div>
+
+                        <br>
+                        <!-- Paragrafo text area (crei un testo con la parola da censurare) -->
+                        <div class="d-flex justify-conten-between">
+                            <label class="align-text-top-text-bottom" for="paragraph" id="titleParag"> Paragrafo </label>
+                            <textarea class="textParag" id="paragraph" name="paragraph" placeholder="Inserisci il Testo"></textarea>
+
+                            <button class="rounded-pill" id="myButton" type="submit" value="Invia">Invia</button>
+                        </div>
                     </div>
-
-                    <br>
-
-                    <div class="censured">
-                        <!-- parola da censurare censurata -->
-                        <input type="text" id="censored_word" name="censored_word" placeholder="Inserisci Censura">
-                        <label for="censored_word"> Parola da censurare </label>
-                    </div>
-
-                    <br>
-                    <!-- Paragrafo text area (crei un testo con la parola da censurare) -->
-                    <div class="d-flex justify-conten-between">
-                        <label class="align-text-top-text-bottom" for="paragraph" id="titleParag"> Paragrafo </label>
-                        <textarea class="textParag" id="paragraph" name="paragraph" placeholder="Inserisci il Testo"></textarea>
-
-                        <button class="rounded-pill" id="myButton" type="submit" value="Invia">Invia</button>
-                    </div>
+                </form>
             </div>
-            </form>
         </div>
-    </div>
     </div>
     <!-- JavaScript CDN Bootstrap -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
